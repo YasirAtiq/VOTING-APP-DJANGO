82f66311df011a789393ab2e9d1b7070b6e40c2e
Yasir <DPSA00609@mypaceschool.com>
2023-11-27 18:35:44 +0400

    Creating Model for Votes.

--- voting_app/models.py
+++ voting_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Votes(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField()
+    date = models.DateField(auto_now=True)
+    choice = models.CharField(max_length=80)
